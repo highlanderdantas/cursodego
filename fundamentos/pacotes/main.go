565522c4376c522c39da0f39122392fdd3cd50ba
Highlander Dantas <haylander60@gmail.com>
2019-07-20 10:58:31 -0300

    first commit

---
+++ fundamentos/pacotes/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"cursodego/fundamentos/pacotes/prefixo"
+	"fmt"
+)
+
+//NomeDoUsuario é o nome do usuário do sistema
+var NomeDoUsuario = "Jeff"
+
+func main() {
+	fmt.Printf("Nome do usuario: %s\r\n", NomeDoUsuario)
+	fmt.Printf("Prefixo da Capital: %d\r\n", prefixo.Capital)
+	// fmt.Printf("Nome da operadora: %s\r\n", operadora.NomeOperadora)
+	// fmt.Printf("Valor de teste: %s\r\n", prefixo.TesteComPrefixo)
+}
---
+++ fundamentos/pacotes/prefixo/numero.go
@@ -0,0 +1,4 @@
+package prefixo
+
+//Capital representa o numero do prefixo de telefone da capital de um estado/provincia
+var Capital = 11
